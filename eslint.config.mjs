3493ae78632711697be37a1dbe1c8a5f3e11ceda
brenno <brennovianacoelho.dev@gmail.com>
2026-09-23 15:32:42 -0300

    feat: some changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ['.serverless/**', '.serverless-offline/**', 'node_modules/**'] },
   eslint.configs.recommended,
   tseslint.configs.recommended,
